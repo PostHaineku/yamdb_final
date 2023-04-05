8ff95db26bb1a9bd0680140bedd74ba0db9917b9
Влад Бочкарёв <ichi222@yandex.ru>
2023-04-05 17:04:38 +0300

    pep8 v2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # yamdb_final
 yamdb_final
-![yamdb_workflow](https://github.com/PostHaineku/yamdb_final/actions/runs/4619023321/badge.svg)
+[![yamdb_workflow](https://github.com/PostHaineku/yamdb_final/actions/workflows/yamdb_workflow.yml/badge.svg)](https://github.com/PostHaineku/yamdb_final/actions/workflows/yamdb_workflow.yml)
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
 from django.db.utils import IntegrityError
-from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
