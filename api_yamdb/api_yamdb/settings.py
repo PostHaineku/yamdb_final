a00231c168613da171a21e60c020d7e69ffc335d
Влад Бочкарёв <ichi222@yandex.ru>
2023-04-05 17:01:05 +0300

    pep8

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', default='localhost'),
         'PORT': os.getenv('DB_PORT', default='')
     }
-} 
+}
 
 
 # Password validation
@@ -123,7 +123,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
